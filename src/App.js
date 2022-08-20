2db7265c64c1bdebc23e1adf512655b35a26212c
MaxwellChrist <ribthulu@gmail.com>
2022-08-20 15:24:37 -0400

    installed tailwind and react-icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import {GiDiscobolus} from 'react-icons/gi'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world <GiDiscobolus />
+      </h1>
     </div>
   );
 }
